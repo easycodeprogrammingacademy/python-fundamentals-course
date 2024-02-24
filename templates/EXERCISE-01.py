4fae2c06e1370446e6bd0b4e0f5f1fed5d37f236
EasycodeAcademy <161119691+EasycodeAcademy@users.noreply.github.com>
2024-02-24 17:26:03 +0800

    Create EXERCISE-01.py

---
+++ templates/EXERCISE-01.py
@@ -0,0 +1,58 @@
+# Question 1
+# Print the following sentence – “The best programming language for 
+# beginners is Python”.
+
+
+# Question 2
+# Print the following sentence in two separate lines - “Python is a 
+# popular programming language. It was created in 1991”. You are 
+# required to use “\n” for this question.
+
+
+# Question 3
+# Define a variable for each of the following datatype:
+# • Positive integer
+# • Negative integer
+# • Positive float with 3 decimal places
+# • Negative float with 1 decimal place
+
+
+# Question 4
+# Create a variable named valueA and set its value to 10. Create 
+# another variable named valueB and set its value to 20. Print the sum, 
+# difference and product of the two values.
+
+
+# Question 5
+valueA, valueB, valueC = 10, 2, 3
+print(valueA / valueB)
+print(valueA // valueB)
+print(valueA // valueC)
+print(valueA % valueB)
+print(valueA % valueC)
+
+
+# Question 6
+# print(math.ceil(3.54))
+# print(math.floor(99.99))
+# print(math.sqrt(25))
+# print(math.pow(2,3))
+# print(math.factorial(5))
+# print(math.pi)
+
+
+# Question 7
+# Write a program to representthe following formulas
+# • Volume of cuboid = length x width x height
+# • Area of circle = π x r2
+
+
+
+
+
+
+
+
+
+
+
